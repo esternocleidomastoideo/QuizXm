b7f43b9992193c5a79b5de6d5fb33c61ac25812c
Marcelo Araujo <marceloaraujo14@hotmail.com>
2016-05-17 21:55:14 -0300

    Implemented Splash Screen with fade in animation

---
+++ app/src/main/java/br/com/mdi/br/quizxm/Screen11.java
@@ -0,0 +1,14 @@
+package br.com.mdi.br.quizxm;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Screen11 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen11);
+    }
+
+}
